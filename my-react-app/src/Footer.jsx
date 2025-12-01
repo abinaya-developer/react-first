77c0407c68db4e92350021864da83ce76cdce6a4
Abinaya <abinayam044@gmail.com>
2025-12-01 18:40:27 +0530

    first commit

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+    return(
+        <div>
+            <p>@copy Rights Reserved|2025 Abinaya</p>
+        </div>
+    );
+
+}
+
+export default Footer;
---
+++ my-react-app/src/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar(){
+    return(
+        <nav className="card">
+            <center>
+            <h2>Navbar</h2>
+            <button>button</button>
+            </center>
+        </nav>
+    );
+}
+
+export default Navbar;
